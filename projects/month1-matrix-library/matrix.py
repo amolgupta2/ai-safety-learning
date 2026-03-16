857d4095b826f92518f831b66f88d93f53b6f116
amolgupta2 <amolguptx@gmail.com>
2026-03-16 16:58:51 -0400

    Update matrix.py
    Adding in the skeleton __init__ and __str__ functions

--- projects/month1-matrix-library/matrix.py
+++ projects/month1-matrix-library/matrix.py
@@ -1,1 +1,9 @@
+class: Matrix
 
+def __init__(self, data):
+  # data is a list of lists
+  # e.g. Matrix([[1, 2], [3, 4]])
+  self.data = data
+
+def __str__(self):
+  return '\n'.join([str(row) for row in self.data])

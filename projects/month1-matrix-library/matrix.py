cfad9b10c987be71215092d38b4223a263fd515b
amolgupta2 <amolguptx@gmail.com>
2026-03-17 00:19:40 -0400

    Update matrix.py
    Added in my __add__ fxn which allows for Matrix Addition without NumPy. This function allowed me to truly understand what goes behind matrix addition that happens so easily in NumPy

--- projects/month1-matrix-library/matrix.py
+++ projects/month1-matrix-library/matrix.py
@@ -7,3 +7,15 @@ def __init__(self, data):
 
 def __str__(self):
   return '\n'.join([str(row) for row in self.data])
+
+def __add__(self, other):
+  if len(self.data) == len(other.data) and len(self.data[0]) == len(other.data[0]):
+    result = [0] * len(self.data) # Defining the result array
+    for i in range(len(self.data)):
+      result[i] = self.data[i].copy()
+    for i in range(len(self.data)):
+        for j in range(len(self.data[0])):
+            result[i][j] = self.data[i][j] + other.data[i][j]
+    return Matrix(result)
+  else:
+    raise ValueError("Matrices must have the same dimensions")

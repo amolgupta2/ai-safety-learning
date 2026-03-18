6fd2c0b50180bd00353712977e0ad75cf26fdbc7
amolgupta2 <amolguptx@gmail.com>
2026-03-18 19:55:49 -0400

    Update test_matrix.py
    Added test cases for Transpose

--- projects/month1-matrix-library/test_matrix.py
+++ projects/month1-matrix-library/test_matrix.py
@@ -17,3 +17,10 @@ print(a * b)  # Expected: [[1, 2], [3, 4]]
 a = Matrix([[1, 2, 3], [4, 5, 6]])
 b = Matrix([[7, 8], [9, 10], [11, 12]])
 print(a * b)  # Expected: [[58, 64], [139, 154]]
+
+# Test transpose()
+a = Matrix([[1, 2], [3, 4], [5, 6]])
+print(a.transpose()) # Expected: [[1, 3, 5], [2, 4, 6]]
+
+b = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9], [0, 0, 0]])
+print(b.transpose()) # Expected: [[1, 4, 7, 0], [2, 5, 8, 0], [3, 6, 9, 0]]

b8df098eb729b54dd3e4f5ee0a3d634c602d792c
amolgupta2 <amolguptx@gmail.com>
2026-03-18 20:11:50 -0400

    Update test_matrix.py
    Test data for shape()

--- projects/month1-matrix-library/test_matrix.py
+++ projects/month1-matrix-library/test_matrix.py
@@ -21,13 +21,11 @@ print(a * b)  # Expected: [[58, 64], [139, 154]]
 # Test transpose()
 a = Matrix([[1, 2], [3, 4], [5, 6]])
 print(a.transpose()) # Expected: [[1, 3, 5], [2, 4, 6]]
-
 b = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9], [0, 0, 0]])
 print(b.transpose()) # Expected: [[1, 4, 7, 0], [2, 5, 8, 0], [3, 6, 9, 0]]
 
 # Test shape()
-a = [[1, 2], [3, 4], [5, 6]]
+a = Matrix([[1, 2], [3, 4], [5, 6]])
 print(a.shape()) # Expected: (3, 2)
-
-b = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [0, 0, 0]]
+b = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9], [0, 0, 0]])
 print(b.shape()) # Expected: (4, 3)
